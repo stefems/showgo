5a23a17ae288446ef027a7c652d57939303c52bf
stefems <kowalmax.s@gmail.com>
2017-06-08 16:10:10 -0400

    f

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '3000';
+const port = '3000';
 app.set('port', port);
 
 /**
